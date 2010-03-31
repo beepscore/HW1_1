3732c9988bef903c13271cc5cccd2a3704369789
beepscore <support@beepscore.com>
2010-03-31 13:30:04 -0700

    Fix Clang warnings.

--- HW1_1AppDelegate.h
+++ HW1_1AppDelegate.h
@@ -10,10 +10,25 @@
 
 @interface HW1_1AppDelegate : NSObject <NSApplicationDelegate>
 {
+#pragma mark Instance variables
     NSWindow *window;
 
 	NSTextField*		textField_;
 }
+
+#pragma mark Properties
+
+// Apple Memory Management Programming Guide for Cocoa recommends
+// on Mac, assign outlet, don't dealloc it.
+// on iPhone, retain outlet, dealloc it.
+// http://developer.apple.com/iphone/library/documentation/Cocoa/Conceptual/MemoryMgmt/Articles/mmNibObjects.html
+// On Mac, I think the .xib file File's Owner retains top level objects.
+//
+// Chris Hanson (from Apple) recommends use a subclass of 
+// NSWindowController or NSViewController (on Mac) or UIViewController (on iPhone).
+// Make a .xib file with the window controller or view controller as File's Owner.
+// Then on Mac or iPhone, retain the IBOutlet properties, and release them in dealloc
+// http://stackoverflow.com/questions/2097822/retain-counts-of-iboutlets
 
 @property (assign) IBOutlet NSWindow*		window;
 @property (assign) IBOutlet NSTextField*	textField;
--- HW1_1AppDelegate.m
+++ HW1_1AppDelegate.m
@@ -16,9 +16,9 @@
 @end
 
 
-
 @implementation HW1_1AppDelegate
 
+#pragma mark Properties
 @synthesize window;
 @synthesize textField = textField_;
 
@@ -26,25 +26,45 @@
 {	
 	// Create an array of numbers and strings
 	
-	NSString* zeroString		= [self newZeroString];
+    // You own any object you create with alloc, new, or copy.  You are responsible for releasing it.
+    // http://developer.apple.com/mac/library/documentation/cocoa/conceptual/MemoryMgmt/Articles/mmObjectOwnership.html#//apple_ref/doc/uid/20000043-BEHDEDDB
+	NSString* zeroString		= [self newZeroString];    
 	NSString* fiveString		= [[NSString alloc] initWithString:@"five"];
 	NSString* twentyThreeString	= [[NSString alloc] initWithString:@"twenty three"];
+    
+    // SB changed
+    // test a string with one character
+	NSString* bString           = [[NSString alloc] initWithString:@"b"];
 
+    // arrayWithObjects returns autoreleased array
 	NSArray* numbersAndStrings = [NSArray arrayWithObjects:zeroString,
 														   [NSNumber numberWithInt:2],
 														   [NSNumber numberWithInt:3],
 														   fiveString,
 														   twentyThreeString,
 														   [NSNumber numberWithInt:117],
+                                                           bString,
 														   nil];
-	[self reverseStringsInArray:numbersAndStrings];						
+    
+    // SB changed
+    // array retained its elements.  Release the strings
+    [zeroString release];
+    [fiveString release];
+    [twentyThreeString release];
+    [bString release];
+
+	[self reverseStringsInArray:numbersAndStrings];
 }
 
 
 - (void) reverseStringsInArray:(NSArray*)objects
 {
+    // NSString class method string returns an autoreleased empty string
 	NSMutableString*	textFieldContent = [NSMutableString string];
-	NSMutableString*	reversedString = nil;
+
+	// SB changed 
+    // I think this got released before we had a chance to use it
+    // NSMutableString*	reversedString = nil;
 	
 	for (NSObject* object in objects)
 	{
@@ -53,33 +73,43 @@
 			NSString* stringToReverse = (NSString*)object;
 			NSUInteger length = [stringToReverse length];
 			
-			reversedString = [[NSMutableString alloc] initWithCapacity:length];
+			// reversedString = [[NSMutableString alloc] initWithCapacity:length];
+			NSMutableString* reversedString = [[NSMutableString alloc] initWithCapacity:length];
 			
 			for (NSInteger i = length - 1; i >= 0; i--)
 			{
 				[reversedString appendFormat:@"%C",[stringToReverse characterAtIndex:i]];
 			}
+            
+            [textFieldContent appendFormat:@"Reversed a string:%@\n", reversedString];
+            [reversedString release];
 		}
-		
-		if ( reversedString )
-		{
-			[textFieldContent appendFormat:@"Reversed a string:%@\n", reversedString];
-		}
-		
-		if ( reversedString != nil )
-		{
-			[reversedString release];
-		}
+
+		// SB changed - move these into if block above
+//		if ( reversedString )
+//		{
+//			[textFieldContent appendFormat:@"Reversed a string:%@\n", reversedString];
+//		}
+//		
+//		if ( reversedString != nil )
+//		{
+//			[reversedString release];
+//		}
 	}
-		
-	[textField_ setStringValue:textFieldContent];
+	
+	// better practice to use property setter instead of instance variable
+    // [textField_ setStringValue:textFieldContent];
+	[self.textField setStringValue:textFieldContent];
 	return;
 }
 
 
+// SB changed
+// By convention, a method whose name starts with "new" returns an object with a retain count of 1.
 - (NSString*) newZeroString
 {
-	return [[[NSString alloc]initWithString:@"zero"] autorelease];	
+    //	return [[[NSString alloc]initWithString:@"zero"] autorelease];	
+	return [[NSString alloc] initWithString:@"zero"];	
 }
 
 @end
